ca8b89fd5a26666d030e5a732eb6a4b406445dbd
peeppeep-io <atillathegunlsrp@gmail.com>
2025-02-05 16:22:35 +0530

    Crafting changes - interior crafting implementation refactoring

--- Los Santos RED/lsr/Player/Player.cs
+++ Los Santos RED/lsr/Player/Player.cs
@@ -177,7 +177,6 @@ namespace Mod
             IntimidationManager = new IntimidationManager(this, World, Settings);
             GamblingManager = new GamblingManager(this, Settings, TimeControllable);
             VehicleManager = new VehicleManager(this, World, Settings);
-            CraftingFlags = new HashSet<string>();
         }
         public IntimidationManager IntimidationManager { get; private set; }
         public CuffManager CuffManager { get; private set; }
@@ -493,7 +492,7 @@ namespace Mod
         public bool IsInPoliceVehicle { get; private set; }
         public Dispatcher Dispatcher { get; set; }
         public bool IsBlockingTraffic { get; set; }
-        public HashSet<string> CraftingFlags { get; set; }
+        public Crafting Crafting { get; set; }
 
 
 
